b43608bcc225f735fd01244f4a0dce5188c16687
xhyrom <generalkubo@gmail.com>
2022-12-03 14:57:09 +0100

    chore: rename README to README.md

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,12 @@ repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://jitpack.io")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
-    compileOnly(files("libs/HyLib-0.1.0-api.jar"))
+    compileOnly("com.github.xHyroM:HyLib:f148c37b")
     compileOnly("me.clip:placeholderapi:2.11.2")
 
     implementation("net.kyori:adventure-text-serializer-plain:4.12.0")
